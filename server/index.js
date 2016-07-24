4af0523662ffd74f2872d4d39d92879a2a9c2bb9
dpocheketa <dpocheketa@gmail.com>
2016-07-24 21:19:32 +0300

    allow crossdomain

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import ejs from 'ejs';
 
+// import passport from 'passport';
+// import LocalStrategy from 'passport-local';
 
 import routes from './routes/index';
 // import users        from './routes/users'
@@ -15,8 +17,9 @@ console.log(__dirname);
 
 const app = express();
 const frontendFolder = path.join(__dirname, '../angular-tutorial');
+const frontendDevFolder = path.join(__dirname, '../../angular-tutorial');
 
-app.set('views', path.join(frontendFolder));
+app.set('views', [frontendDevFolder, frontendFolder]);
 app.engine('html', ejs.renderFile);
 app.set('view engine', 'html');
 
@@ -25,7 +28,55 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(frontendFolder));
+app.use(express.static(frontendDevFolder));
+
+let allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+};
+
+app.use(allowCrossDomain);
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+
 app.use('/', routes);
+
+// var User = function(){
+//   this.findOne = function(obj, cb){
+//     return cb(null, {id: 1});
+//   };
+// };
+
+// passport.use(new LocalStrategy.Strategy(
+//   function(username, password, done) {
+//     User.findOne({ username: username }, function (err, user) {
+//       if (err) { return done(err); }
+//       if (!user) {
+//         return done(null, false, { message: 'Incorrect username.' });
+//       }
+//       if (!user.validPassword(password)) {
+//         return done(null, false, { message: 'Incorrect password.' });
+//       }
+//       return done(null, user);
+//     });
+//   }
+// ));
+
+// app.post('/login', passport.authenticate('local', { successRedirect: '/',
+//                                                     failureRedirect: '/' }));
+
+
+
+
+
+
+
+
+
 
 const server = app.listen(process.env.PORT || 5000, () => {
 
