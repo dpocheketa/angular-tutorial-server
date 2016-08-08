b4cc62fb6a41dbea69d6d3bbecb16538a293d15e
dpocheketa <dpocheketa@gmail.com>
2016-08-08 11:02:34 +0300

    add throng

--- server/index.js
+++ server/index.js
@@ -7,52 +7,59 @@ import logger from 'morgan';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import ejs from 'ejs';
-import mongoose from './libs/mongoose';
 import models from './models';
-import cookieSession from 'cookie-session';
 import passport from 'passport';
 import expressSession from 'express-session';
-
 import routes from './routes/index';
+import throng from 'throng';
 
-const app = express();
-const frontendFolder = path.join(__dirname, '../angular-tutorial');
-const frontendDevFolder = path.join(__dirname, '../../angular-tutorial');
+const WORKERS = process.env.WEB_CONCURRENCY || 1;
 
-app.set('views', [frontendDevFolder, frontendFolder]);
-app.engine('html', ejs.renderFile);
-app.set('view engine', 'html');
-
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(expressSession({
-  secret: 'secret',
-  saveUninitialized: true,
-  resave: true
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.static(frontendFolder));
-app.use(express.static(frontendDevFolder));
-
-let allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-    next();
-};
-
-app.use(allowCrossDomain);
-app.use('/', routes);
-
-const server = app.listen(process.env.PORT || 5000, () => {
-
-  const {address, port} = server.address();
-
-  console.log(`App listening at http://${address}:${port}`);
+throng(start, {
+  workers: WORKERS,
+  lifetime: Infinity
 });
+
+function start(){
+  const app = express();
+  const frontendFolder = path.join(__dirname, '../angular-tutorial');
+  const frontendDevFolder = path.join(__dirname, '../../angular-tutorial');
+
+  app.set('views', [frontendDevFolder, frontendFolder]);
+  app.engine('html', ejs.renderFile);
+  app.set('view engine', 'html');
+
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(expressSession({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(express.static(frontendFolder));
+  app.use(express.static(frontendDevFolder));
+
+  let allowCrossDomain = function(req, res, next) {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+      next();
+  };
+
+  app.use(allowCrossDomain);
+  app.use('/', routes);
+
+  const server = app.listen(process.env.PORT || 5000, () => {
+
+    const {address, port} = server.address();
+
+    console.log(`App listening at http://${address}:${port}`);
+  });
+}
 
 module.exports = app;
