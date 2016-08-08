8c99fc4b06db7dfc31e9502a071c58b9c0c8a3c8
dpocheketa <dpocheketa@gmail.com>
2016-08-09 00:23:33 +0300

    add dependencies

--- server/libs/mongoose.js
+++ server/libs/mongoose.js
@@ -4,6 +4,7 @@ let mongoose = require('mongoose');
 let db = mongoose.connection;
 mongoose.Promise = global.Promise;
 let mongodbUri = process.env.MONGODB_URI || 'mongodb://localhost/test';
+
 mongoose.connect(mongodbUri);
 
 db.on('error', console.error.bind(console, 'connection error:'));
